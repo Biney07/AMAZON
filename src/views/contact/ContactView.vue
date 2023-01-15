9e6df15c4ffb3f1436460ebf59fdea70beef6e93
EnesHetemi <eneshetemi3@gmail.com>
2023-01-15 15:33:57 +0100

    Fronti i Kontaktit i perfunduar

--- src/views/contact/ContactView.vue
+++ src/views/contact/ContactView.vue
@@ -1,29 +1,32 @@
 <template>
 
 
- <div class="container py-4 w-25">
-  <h2 style="color:green;">Kontakto</h2>
-  <form id="contactForm">
+ <div class="container py-4 w-50">
+  <h2 style="color:rgba(126, 239, 104, 0.8);">Kontakto</h2>
+  <form id="contactForm" v-on:submit.prevent="handleCreateKontakt">
 
     <div class="mb-3">
       <label class="form-label" for="name">Emri</label>
-      <input class="form-control" id="name" type="text" placeholder="Name" data-sb-validations="required" />
+      <input class="form-control" v-bind:value="newKontakt.emri"
+                v-on:input="newKontakt.emri = $event.target.value" type="text" placeholder="Emri" data-sb-validations="required" />
     </div>
 
     <div class="mb-3">
       <label class="form-label" for="emailAddress">Email</label>
-      <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required, email" />
+      <input class="form-control" v-bind:value="newKontakt.email"
+                v-on:input="newKontakt.email = $event.target.value" type="email" placeholder="Email" data-sb-validations="required, email" />
     </div>
 
     <div class="col-md-12">
       <div class="form-group">
-        <label class="label" for="#">Message</label>
-        <textarea name="message" class="form-control" id="message" cols="30" rows="4" placeholder="Message"></textarea>
+        <label class="label" for="#">Mesazhi</label>
+        <textarea name="message" class="form-control" v-bind:value="newKontakt.mesazhi"
+                v-on:input="newKontakt.mesazhi = $event.target.value" cols="30" rows="4" placeholder="Mesazhi"></textarea>
       </div>
     </div>
 
     <div class="d-grid mt-4">
-      <button class="btn btn-primary btn-lg" style="background-color:green" id="submitButton" type="submit">Submit</button>
+      <button class="btn btn-lg" id="submitButton" type="submit">Dergo</button>
     </div>
 
   </form>
@@ -38,17 +41,17 @@
 
             <div class="info-wrap">
               <div class="row">
-                <div class="col-lg-4 info shadow p-3 mb-5 bg-white rounded">
+                <div class="col-lg-4 info shadow p-3 mb-5 rounded">
                   <h4>Lokacioni:</h4>
                   <p>Kilometrin 8-tė tė magjistrales<br>Prishtinė – Mitrovicė</p>
                 </div>
 
-                <div class="col-lg-4 info mt-4 mt-lg-0 shadow p-3 mb-5 bg-white rounded">
+                <div class="col-lg-4 info mt-4 mt-lg-0 shadow p-3 mb-5 rounded">
                   <h4>Email:</h4>
                   <p>info@amazonahotel.com</p>
                 </div>
 
-                <div class="col-lg-4 info mt-4 mt-lg-0 shadow p-3 mb-5 bg-white rounded">
+                <div class="col-lg-4 info mt-4 mt-lg-0 shadow p-3 mb-5 rounded">
                   <h4>Telefoni:</h4>
                   <p>+38344111222</p>
                 </div>
@@ -61,11 +64,44 @@
 </div>
   </template>
 
+  <script>
+  export default {
+        data() {
+            return {
+                newKontakt: {
+                    emri: '',
+                    email: '',
+                    mesazhi: ''
+                },
+            }
+        },
+        methods: {
+            async handleCreateKontakt() {
+                console.log('Kontakti qe eshte derguar -> ', this.newKontakt);
+                
+                this.newKontakt.emri = '';
+                this.newKontakt.email = '';
+                this.newKontakt.mesazhi = '';
+            }
+        }
+  }
+  </script>
 
-  <style>
-  .rounded:hover{
-    color: rgba(126, 239, 104, 0.8);
-    cursor: pointer;
+
+  <style scoped>
+  button, .info{
+    background-color: rgba(126, 239, 104, 0.8);
+    color: white;
+  }
+
+  button:hover{
+    background-color: rgba(95, 214, 71, 0.8);
+    color: black;
+  }
+
+  .info:hover{
+    background-color: rgba(95, 214, 71, 0.8);
+    color: black;
   }
 
   input, textarea{
