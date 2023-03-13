44c17455b55522e3aedaabfc5a488809141292f3
Blendhasani <93322486+Blendhasani@users.noreply.github.com>
2023-03-13 23:33:12 +0100

    List of users and Delete user action

--- src/views/dashboard/register/Register.vue
+++ src/views/dashboard/register/Register.vue
@@ -1,50 +1,105 @@
 <template>
-     <div class="m-5">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead class="table-info">
-                        <tr>
-                            <th>Emri</th>
-                            <th>Email</th>
-                            <th>Roli</th>
-                            <th>Veprim</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(user, idx) in users" :key="idx">
-                            <td>   {{ user.displayName }}</td>
-                            <td>   {{ user.email }}</td>
-                            <td class="py-3 px-2" v-if="user.customClaims.user == true">User</td>
-                          
-                            <td><button class="btn btn-sm btn-success"><router-link class="btn-success" :to="`/restaurantdasmat/edit/${dasmat._id}`" >Edit</router-link></button> | <button class="btn btn-sm btn-danger" @click="handleDeleteDasmat(dasmat._id)">Delete</button></td>
+    <h1 class="mt-3">All Users</h1>
+  <div class="table-container">
+      <table class="table table-striped">
+        <thead class="table-dark">
+          <tr>
+            <th>User Id</th>
+            <th>Role</th>
+            <th>Full Name</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in filteredUsers" :key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.role }}</td>
+            <td>{{ user.userName}}</td>
+           
+           
+            <td>
+              <button class="btn btn-danger"  @click="deleteUser(user.id)">Delete User</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  <style>
+.table-container {
+  display: flex;
+  justify-content: center;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-top: 30px;
+}
+</style>
+  <script>
+import { collection, getDocs, doc, deleteDoc} from "firebase/firestore";
+import { db } from "@/firebase/index";
 
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-          
-        </div>
-</template> 
 
   
-  <script>
-  import { mapState } from 'vuex'
   export default {
-      data(){
-          return{
-              claim: 'user'
-          }
-      },
-      methods:{
-          handleDeleteUser(id){
-              this.$store.dispatch('user/deleteUser', id)
-          }
-      },
-      computed:{
-        ...mapState(["users"]),
-      },
-      mounted(){
-          this.$store.dispatch('usersByClaim', this.claim);
-      },
+    data() {
+      return {
+        users: [],
+     
+        
+      }
+    },
+    computed: {
+  filteredUsers() {
+    return this.users.filter(user => user.userName);
   }
-  </script>
+},
+    created() {
+
+
+     const getUsers = async () => {
+  const usersCollection = collection(db, "users");
+  const querySnapshot = await getDocs(usersCollection);
+  const users = [];
+
+  querySnapshot.forEach(async (doc) => {
+    const data = doc.data();
+    const user = {
+      id: doc.id,
+      role: data.role,
+      userName: data.name,
+    
+    };
+
+   
+
+    users.push(user);
+  });
+
+  return users;
+};
+
+    // Call getUsers function and log the users
+    getUsers().then((users) => {
+    console.log(users);
+    this.users = users;
+    }).catch((error) => {
+    console.log(error);
+    });
+    },
+    methods:{
+        async deleteUser(userId) {
+          try {
+            const userRef = doc(db, "users", userId);
+            await deleteDoc(userRef);
+            const updatedUsers = this.users.filter((user) => user.id !== userId);
+            this.users = updatedUsers;
+            console.log(`User with ID ${userId} has been deleted`);
+          } catch (error) {
+            console.error(error);
+          }
+        },
+      },
+   }
+
+
+</script>
